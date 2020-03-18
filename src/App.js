4f2cad982ebac02afd070cfd0573bdb6f128bd73
sofiadavydiuk <sofiadavydyuk@gmail.com>
2020-03-18 18:05:04 +0100

    test-task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FetchCompanies from "./FetchCompanies";
+import {Input, Header, Container, Segment, Search} from 'semantic-ui-react';
+import MapExample from "./MapExample";
+
+const numbers = [1, 2, 3, 4, 5];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Search />
+        <header className="App-header">
+          <Input placeholder='Search...' />
+
+
+            <FetchCompanies />
+
+
+          <MapExample numbers={numbers} />
+          <img src={logo} className="App-logo" alt="logo" />
+
+        </header>
+
+      </div>
   );
 }
 
 export default App;
+
+// To do:
+// 1) map of companies
+// 2) redairect with one company info
+// 3) styles
+// 4) filtry
+// 5) github
+// 6) trello
